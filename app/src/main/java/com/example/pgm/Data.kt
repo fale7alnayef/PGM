b17b8c70d8caae64e8e80f0d0d3f0dd34181aa50
Ghassan Al-Mahameed <ghassanmah@gmail.com>
2022-08-11 19:08:11 +0300

    editing the sub using api

--- app/src/main/java/com/example/pgm/Data.kt
+++ app/src/main/java/com/example/pgm/Data.kt
@@ -8,7 +8,7 @@ class Data {
         lateinit var img: String
         lateinit var gymName: String
 
-        val url = "192.168.1.110"
+        val url = "192.168.137.1"
 
 
     }
--- app/src/main/java/com/example/pgm/UpdateTraineeActivity.kt
+++ app/src/main/java/com/example/pgm/UpdateTraineeActivity.kt
@@ -2,16 +2,20 @@ package com.example.pgm
 
 
 import android.app.Activity
-import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import de.hdodenhof.circleimageview.CircleImageView
-import java.text.SimpleDateFormat
+import org.json.JSONObject
 import java.util.*
 
 
@@ -25,28 +29,24 @@ class UpdateTraineeActivity : AppCompatActivity() {
     private lateinit var emailContainer: TextInputLayout
     private lateinit var passwordContainer: TextInputLayout
     private lateinit var confirmPasswordContainer: TextInputLayout
-    private lateinit var birthdayContainer: TextInputLayout
     private lateinit var numberContainer: TextInputLayout
-    private lateinit var firstNameContainer: TextInputLayout
-    private lateinit var lastNameContainer: TextInputLayout
     private lateinit var weightContainer: TextInputLayout
     private lateinit var heightContainer: TextInputLayout
 
     private lateinit var email: TextInputEditText
     private lateinit var password: TextInputEditText
     private lateinit var confirmPassword: TextInputEditText
-    private lateinit var birthday: TextInputEditText
     private lateinit var number: TextInputEditText
-    private lateinit var firstName: TextInputEditText
-    private lateinit var lastName: TextInputEditText
     private lateinit var weight: TextInputEditText
     private lateinit var height: TextInputEditText
-
+    lateinit var id: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update_trainee)
+
+        id = intent.extras?.get("id").toString()
 
         pickImage = findViewById(R.id.updatepickTraineeImage)
 
@@ -64,17 +64,13 @@ class UpdateTraineeActivity : AppCompatActivity() {
         confirmPassword = findViewById(R.id.updateconfirmPasswordTraineeEditText)
         confirmPasswordContainer = findViewById(R.id.updateconfirmPasswordTraineeContainer)
 
-        birthday = findViewById(R.id.updatebirthDayTraineeEditText)
-        birthdayContainer = findViewById(R.id.updatebirthDayTraineeContainer)
+
 
         number = findViewById(R.id.updatephoneNumberTraineeEditText)
         numberContainer = findViewById(R.id.updatephoneNumberTraineeContainer)
 
-        firstName = findViewById(R.id.updatefirstNameTraineeEditText)
-        firstNameContainer = findViewById(R.id.updatefirstNameTraineeContainer)
 
-        lastName = findViewById(R.id.updatelastNameTraineeEditText)
-        lastNameContainer = findViewById(R.id.updatelastNameTraineeContainer)
+
 
         weight = findViewById(R.id.updateweightTraineeEditText)
         weightContainer = findViewById(R.id.updateweightTraineeContainer)
@@ -84,6 +80,57 @@ class UpdateTraineeActivity : AppCompatActivity() {
 
         submit.setOnClickListener {
 
+            val queue = Volley.newRequestQueue(applicationContext)
+            val url = "http://${Data.url}:8000/api/admin/edit_user/$id"
+
+            val jsonBody = JSONObject()
+
+            try {
+                jsonBody.put("email", email.text.toString())
+            } catch (e: Exception) {
+                Log.e("email", e.toString())
+            }
+
+            try {
+                jsonBody.put("phone_number", number.text.toString())
+            } catch (e: Exception) {
+                Log.e("phone_number", e.toString())
+            }
+
+            try {
+                jsonBody.put("password", password.text.toString())
+            } catch (e: Exception) {
+                Log.e("password", e.toString())
+            }
+
+            try {
+                jsonBody.put("weight", weight.text.toString())
+            } catch (e: Exception) {
+                Log.e("weight", e.toString())
+            }
+
+            try {
+                jsonBody.put("height", height.text.toString())
+            } catch (e: Exception) {
+                Log.e("height", e.toString())
+            }
+
+
+            val jsonObjectRequest = JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                jsonBody,
+                {
+                    Toast.makeText(applicationContext, "edited", Toast.LENGTH_SHORT).show()
+                    finish()
+                },
+                {
+                    Toast.makeText(applicationContext, it.networkResponse.headers?.get("message").toString(), Toast.LENGTH_SHORT).show()
+
+                }
+            )
+            queue.add(jsonObjectRequest)
+
         }
 
 
@@ -91,36 +138,9 @@ class UpdateTraineeActivity : AppCompatActivity() {
             pickImageFromGallery()
         }
 
-        initDatePicker()
 
     }
 
-    private fun initDatePicker() {
-        calendar = Calendar.getInstance()
-
-        val datePicker = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
-            calendar.set(Calendar.YEAR, year)
-            calendar.set(Calendar.MONTH, month)
-            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-            updateLapel(calendar)
-
-        }
-        birthday.setOnClickListener {
-            DatePickerDialog(
-                this,
-                datePicker,
-                calendar.get(Calendar.YEAR),
-                calendar.get(Calendar.MONTH),
-                calendar.get(Calendar.DAY_OF_MONTH)
-            ).show()
-        }
-    }
-
-    private fun updateLapel(calendar: Calendar) {
-
-        val sdf = SimpleDateFormat("dd-MM-yyyy", Locale.UK)
-        birthday.setText(sdf.format(calendar.time))
-    }
 
     private fun pickImageFromGallery() {
         val intent = Intent(Intent.ACTION_PICK)
