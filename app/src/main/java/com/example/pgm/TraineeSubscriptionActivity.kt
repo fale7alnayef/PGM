160987f093b055b5587a561b356598e832a9515e
Ghassan Al-Mahameed <ghassanmah@gmail.com>
2022-08-12 18:17:10 +0300

    show payments with api from trainee

--- app/src/main/java/com/example/pgm/TraineeSubscriptionActivity.kt
+++ app/src/main/java/com/example/pgm/TraineeSubscriptionActivity.kt
@@ -1,12 +1,18 @@
 package com.example.pgm
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.material.button.MaterialButton
 
 class TraineeSubscriptionActivity : AppCompatActivity() {
+    lateinit var subID:String
     private lateinit var startDate: TextView
     private lateinit var endDate: TextView
     private lateinit var payment: TextView
@@ -26,13 +32,60 @@ class TraineeSubscriptionActivity : AppCompatActivity() {
         privatec = findViewById(R.id.traineePrivateSubs)
         payments = findViewById(R.id.traineePayments)
 
+
+        val queue = Volley.newRequestQueue(applicationContext)
+        val url = "http://${Data.url}:8000/api/user/show_sub"
+        val token = "Bearer " + Data.Token
+
+        val dataRequest = @RequiresApi(Build.VERSION_CODES.O)
+        object : JsonObjectRequest(
+            Method.POST,
+            url,
+            null,
+            {
+                startDate.text = it.getString("starts_at")
+                endDate.text = it.getString("ends_at")
+                subID=it.getString("id")
+                val private = it.getString("private")
+                val fulPaid = it.getString("fully_paid")
+
+                if (private.equals("0")) {
+                    privatec.text = "NO"
+                } else {
+                    privatec.text = "Yes"
+                }
+
+                if (fulPaid.equals("false")) {
+                    fullyPaid.text = "NO"
+                } else {
+                    fullyPaid.text = "YES"
+                }
+
+                coach.text = it.getString("coach_name")
+                payment.text = it.getString("price")
+            },
+            {
+
+            }
+        ) {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Authorization"] = token
+                return headers
+            }
+        }
+        queue.add(dataRequest)
+
+
         payments.setOnClickListener {
             navigateToPayment()
         }
     }
 
     private fun navigateToPayment() {
-        startActivity(Intent(applicationContext, PaymentsActivity::class.java))
+        val i = Intent(applicationContext, PaymentsActivity::class.java)
+        i.putExtra("sub_id",subID)
+        startActivity(i)
 
     }
 
